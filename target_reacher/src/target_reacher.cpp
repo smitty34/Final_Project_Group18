39afbdb4be5d893fd901377ba9d7ac093c60067f
smitty34 <93785693+smitty34@users.noreply.github.com>
2022-12-10 16:19:11 -0500

    Update target_reacher.cpp

--- target_reacher/src/target_reacher.cpp
+++ target_reacher/src/target_reacher.cpp
@@ -42,6 +42,9 @@ public:
 
         marker_found = create_subscription<ros2_aruco_interfaces::msg::ArucoMarkers>("/waypoints", 10, std::bind(&TargetReacher::Arucocallback, this, std::placeholders::_1));
       
+        image_detector = create_subscription<sensor_msgs::msg::Image>("/robot1/camera_rgb_frame", 10, std::bind(&TargetReacher::img_finder, this, std::placeholders::_1));
+
+      
     }
 
 
@@ -128,9 +131,82 @@ private:
             
 
 
+    }
+    
+    void img_finder(sensor_msgs::msg::Image::ConstSharedPtr image_msg)
+    {
+        rclcpp::Time now;
+
+        
+        ros2_aruco_interfaces::msg::ArucoMarkers ar_tag;
+
+        geometry_msgs::msg::TransformStamped trans_img;
+
+        ar_tag.header.frame_id = image_msg->header.frame_id;
+        
+        
+        cv_bridge::CvImagePtr cv_ptr;
+
+        try
+        {
+            {
+                cv_ptr = cv_bridge::toCvCopy(image_msg, sensor_msgs::image_encodings::BGR8);
+            }
+        }
+        catch(cv_bridge::Exception& e)
+        {
+            RCLCPP_ERROR(this->get_logger(), "cv_bridge exception: %s", e.what());
+            return;
+        }
+        
+        auto image = cv_ptr->image.clone();
+        
+        cv::Mat imageCopy;
+
+        cv_ptr->image.copyTo(image);
+        image.copyTo(imageCopy);
+
+        std::vector<int> ids;
+        std::vector<std::vector<cv::Point2f>> corners;
+
+        cv::aruco::detectMarkers(image, dictionary, corners, ids);
+       
+
+        int count = 0;
+       
+
+        for (unsigned int i = 0; i< ids.size(); i++) {
+            RCLCPP_DEBUG(this->get_logger(), "Seen ARUCO marker number %i", ids[i]); 
+            
+            if(ids[i] == 0) {
+                count++;
+
+            }
+        }
+
+        if (count > 0){
+
+
+            ar_tag.header.stamp = now;
+            ar_tag.header.frame_id = "robot1/camera_rgb_optical_frame";
+
+            trans_img.header.frame_id = "robot1/camera_rgb_frame";
+            trans_img.child_frame_id = ar_tag.header.frame_id;
+            trans_img.transform.translation.x = ar_tag.marker_ids.at(x_goal);
+            trans_img.transform.translation.y = ar_tag.marker_ids.at(y_goal);
+
+            tf_broadcaster_->sendTransform(trans_img);
+
+           
+            
+        }
+
+        
 
 
     }
+
+
 
 
     void scan_n_search()
@@ -146,18 +222,9 @@ private:
 
     }
 
-    void go_to_finalgoal()
-    {
 
 
-
-    }
-
-
-
-
-
-    // attributes
+        // attributes
     std::shared_ptr<BotController> m_bot_controller;
     double x_0 = 0;
     double y_0 = 0;
@@ -172,9 +239,19 @@ private:
     bool found_tag = false;
     bool goal_reached = false;
 
+    cv::Ptr<cv::aruco::Dictionary> dictionary;
+
+    std::string marker_frame;
+    std::string camera_frame;
+    std::string reference_frame;
+
+    int target_number = 14;
+
     rclcpp::Subscription<ros2_aruco_interfaces::msg::ArucoMarkers>::SharedPtr marker_found;
+    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_detector;
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr move_bot;
     rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr final_goal;
+    std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
 
 };
 
